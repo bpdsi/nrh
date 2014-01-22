49e6aaeb3ee23d28b2273cf7c19904b5e6e14d3b
thanaphutBenz@gmail.com <thanaphutBenz@gmail.com@be1b412a-3b68-e0f2-b677-66f1229cad3b>
2014-01-22 04:14:24 +0000

    Back Office Implementing

---
+++ admin/adminEdit.php
@@ -0,0 +1,41 @@
+<?php
+	include "../function/functionPHP.php";
+	
+	$CitizenID=$_POST["CitizenID"];
+	$Prefix=$_POST["Prefix"];
+	$PersonName=$_POST["Name"];
+	$Telephone=$_POST["Telephone"];
+	$Email=$_POST["Email"];
+	$BirthTemp=explode("/", $_POST["BirthDate"]);
+	$BirthDate=($BirthTemp[2]-543)."-".$BirthTemp[1]."-".$BirthTemp[0];
+	
+	
+	$AdminID=$_POST["AdminID"];
+	
+	$NameTemp=explode(" ", $PersonName);
+	if(count($NameTemp)==2){
+		$GivenName=$NameTemp[0];
+		$FamilyName=$NameTemp[1];
+		$MiddleName='';
+	}else{
+		$GivenName=$NameTemp[0];
+		$MiddleName=$NameTemp[1];
+		$FamilyName=$NameTemp[2];
+	}
+	
+	$query="
+		update	admin
+		set		CitizenID='$CitizenID',
+				Prefix='$Prefix',
+				PersonName='$PersonName',
+				GivenName='$GivenName',
+				MiddleName='$MiddleName',
+				FamilyName='$FamilyName',
+				Telephone='$Telephone',
+				Email='$Email',
+				BirthDate='$BirthDate'
+		where	AdminID='$AdminID'
+	";
+	$result=mysql_query($query);
+	
+	echo "complete";
