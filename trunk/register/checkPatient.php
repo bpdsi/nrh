35dcee0cf5205bd7b1665d48155dfb212ddb6d2c
thanaphutBenz@gmail.com <thanaphutBenz@gmail.com@be1b412a-3b68-e0f2-b677-66f1229cad3b>
2014-01-27 09:04:33 +0000

    Lock duplicate patient.

---
+++ trunk/register/checkPatient.php
@@ -0,0 +1,19 @@
+<?php
+	include "../function/functionPHP.php";
+	noCache();
+	host("nrh");
+	$HospCode=$_POST["HospCode"];
+	$HospitalNumber=$_POST["HospitalNumber"];
+
+	$query="
+		select	*
+		from	hospital_patient
+		where	HospCode='$HospCode' and
+				HospitalNumber<>'$HospitalNumber'
+	";
+	$result=mysql_query($query);
+	if(mysql_num_rows($result)>0){
+		echo "current";
+	}else{
+		echo "new";
+	}
